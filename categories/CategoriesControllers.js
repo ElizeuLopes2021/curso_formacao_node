94c40329187f6f2cc8e9e238275c810cbdf7a01a
Elizeu Lopes <loopsdesenvolvedor@gmail.com>
2022-07-08 01:21:15 -0300

    Add router and form category

--- categories/CategoriesControllers.js
+++ categories/CategoriesControllers.js
@@ -1,8 +1,33 @@
 const express = require("express");
 const router = express.Router();
+const slugify = require("slugify");
+const Category = require("./Category");
 
 router.get("/admin/categories/new", (req, res) => {
   res.render("admin/categories/new");
+});
+
+router.post("/categories/save", (req, res) => {
+  var title = req.body.title;
+
+  if (!isNaN(title)) {
+    //não é um número
+    res.redirect("/admin/categories/new");
+  } else if (title === undefined) {
+    // indefinido
+    res.redirect("/admin/categories/new");
+  } else {
+    Category.create({
+      title: title,
+      slug: slugify(title),
+    })
+      .then(() => {
+        res.redirect("/");
+      })
+      .catch(() => {
+        res.redirect("admin/categories/new");
+      });
+  }
 });
 
 module.exports = router;
